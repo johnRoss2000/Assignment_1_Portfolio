188a94a92da3ea2cfcb08457c89ba4ce77fab6e2
johnRoss2000 <djross013@gmail.com>
2020-10-09 15:28:47 -0400

    Organizing Files

---
+++ public/Scripts/app.js
@@ -0,0 +1,17 @@
+// IIFE -- Immediately Invoked Function Expression
+
+(function () {
+
+  function Start() {
+    console.lof("App Started...");
+  }
+
+  window.addEventListener("load", Start);
+
+})();
+
+
+
+
+
+
